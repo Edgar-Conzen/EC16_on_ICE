4005c82234bbfec8110c4ac79d6653966576724c
Edgar Conzen <Edgar-Conzen@users.noreply.github.com>
2024-02-01 01:11:14 +0100

    Two pictures added

--- Radiant/ec16_on_ice/impl_1/pll_25_20.v
+++ Radiant/ec16_on_ice/impl_1/pll_25_20.v
@@ -1,5 +1,5 @@
 // Verilog netlist produced by program LSE 
-// Netlist written on Wed Jan 31 23:56:30 2024
+// Netlist written on Thu Feb  1 00:51:58 2024
 // Source file index table: 
 // Object locations will have the form @<file_index>(<first_ line>[<left_column>],<last_line>[<right_column>])
 // file 0 "c:/lscc/radiant/2023.1/ip/avant/fifo/rtl/lscc_fifo.v"
